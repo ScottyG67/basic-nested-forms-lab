e06a9bb2977a3322fc42ba76a3354e210f053b7d
github username <scott.gloyna01@gmail.com>
2021-02-06 14:11:34 -0600

    Done.

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
   def show
     @recipe = Recipe.find(params[:id])
+
   end
 
   def index
@@ -9,8 +10,19 @@ class RecipesController < ApplicationController
 
   def new
     @recipe = Recipe.new
+    @recipe.ingredients.build
+    @recipe.ingredients.build
   end
 
   def create
+    recipe = Recipe.new(strong_params)
+    recipe.save
+    redirect_to recipes_path
+  end
+
+  private
+
+  def strong_params
+    params.require(:recipe).permit(:title,ingredients_attributes:[:name, :quantity])
   end
 end
